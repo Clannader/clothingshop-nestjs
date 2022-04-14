440d4c32d362d14ccc50438bb16df5f201c991fa
oliver <294473343@qq.com>
2022-04-14 22:49:26 +0800

    修改代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger'
 
 @Controller('/')
+@ApiExcludeController(true) // 排除这controller不在swagger显示
 export class AppController {
 
   @Get(['/index$', '/index/*'])
