d66a756f405ec2b5f4204b0a7573926050a59f06
oliver <294473343@qq.com>
2022-04-23 15:04:06 +0800

    修改代码

--- src/public/decorator/common.decorator.ts
+++ src/public/decorator/common.decorator.ts
@@ -1,13 +1,34 @@
 import { applyDecorators } from '@nestjs/common';
 import {
-  ApiHeader,
+  ApiHeaders
 } from '@nestjs/swagger';
 
 export function ApiCommonHeader() {
   return applyDecorators(
-    ApiHeader({
-      name: 'credential',
-      description: '用户凭证'
-    })
+    ApiHeaders([
+      {
+        name: 'Content-Type',
+        description: '上下文协议',
+        required: true,
+        // 由于这个版本的swagger根本不支持default的值,只能使用enum写一个默认值上去了
+        enum: ['application/json'],
+      },
+      {
+        name: 'X-Requested-With',
+        description: '固定请求头',
+        required: true,
+        enum: ['XMLHttpRequest'],
+      },
+      {
+        name: 'credential',
+        description: '用户凭证',
+        required: true,
+      },
+      {
+        name: 'language',
+        description: '用户语言',
+        enum: ['zh', 'en'],
+      },
+    ])
   );
 }
