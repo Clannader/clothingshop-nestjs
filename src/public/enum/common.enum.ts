06262aaf8dcaf5e425df1e98134a34aa8ec5726b
oliver <294473343@qq.com>
2022-04-28 22:18:42 +0800

    修改代码

--- src/public/enum/common.enum.ts
+++ src/public/enum/common.enum.ts
@@ -1,10 +1,10 @@
 export enum CodeEnum {
-  SUCCESS = 100, // 成功响应时返回该响应码
-  EMPTY = 101, // 校验字段为空时返回该响应码
-  FAIL = 102, // 统一失败时返回该状态码
-  INVALID_SESSION = 103, // session无效时返回该响应码
-  NO_RIGHTS = 104, // 没有权限时返回该响应码
-  EXCEPTION = 105, // 发生异常时返回该响应码
-  INVALID_SYS_SETUP = 106, // 系统参数未开启时返回该响应码
-  UNKNOWN = 999, // 发生未知错误时返回该响应码
+  SUCCESS = 1000, // 成功响应时返回该响应码
+  EMPTY = 1001, // 校验字段为空时返回该响应码
+  FAIL = 1002, // 统一失败时返回该状态码
+  INVALID_SESSION = 1003, // session无效时返回该响应码
+  NO_RIGHTS = 1004, // 没有权限时返回该响应码
+  EXCEPTION = 1005, // 发生异常时返回该响应码
+  INVALID_SYS_SETUP = 1006, // 系统参数未开启时返回该响应码
+  UNKNOWN = 9999, // 发生未知错误时返回该响应码
 }
