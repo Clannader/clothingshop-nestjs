89afc3f0ae47195cca76e146e9f47527413f0471
oliver <294473343@qq.com>
2022-04-30 23:38:16 +0800

    修改代码

--- src/user/dto/resp/resp-user-search.dto.ts
+++ src/user/dto/resp/resp-user-search.dto.ts
@@ -1,4 +1,14 @@
-import { RespPageSchemaDto } from '../../../common';
+import { CommonResult } from '../../../common';
 import { UserSchemaDto } from '../user-schema.dto';
 
-export class RespUserSearchDto extends RespPageSchemaDto<UserSchemaDto> {}
+export class RespUserSearchDto extends CommonResult {
+  /**
+   * 总数
+   */
+  total: number;
+
+  /**
+   * 用户列表
+   */
+  users: UserSchemaDto[];
+}
