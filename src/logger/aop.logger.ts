190c28bfb5da2ea9a15c9e3c40ee606b4ed683f5
oliver <294473343@qq.com>
2022-05-01 10:34:44 +0800

    修改代码

--- src/logger/aop.logger.ts
+++ src/logger/aop.logger.ts
@@ -2,7 +2,12 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class AopLogger extends ConsoleLogger {
   log(message: any, context?: string) {
-    // console.log(context); // 把这个logger加入了app里面,就不打印执行时间了,不知道为什么
-    super.log(message, context);
+    // 把这个logger加入了app里面,就不打印执行时间了,不知道为什么
+    // 我现在知道为什么不打印时间了,是要设置logger参数才可以的
+    if (context) {
+      super.log(message, context);
+    } else {
+      super.log(message);
+    }
   }
 }
