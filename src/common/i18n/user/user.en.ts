accd7ff2e58455337d15d58c6674b7b79e06ec6b
oliver <294473343@qq.com>
2022-05-07 20:54:46 +0800

    修改代码

--- src/common/i18n/user/user.en.ts
+++ src/common/i18n/user/user.en.ts
@@ -1,3 +1,5 @@
 export const user = {
   userName: 'Please input your user name',
+  userTest: 'Testing',
+  userTestArgs: 'Testing {0}'
 };
--- src/common/utils/global.service.spec.ts
+++ src/common/utils/global.service.spec.ts
@@ -15,4 +15,28 @@ describe('GlobalService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('测试英文翻译', () => {
+    expect(service.lang('EN', '测试', 'user.userTest')).toBe('Testing')
+  });
+
+  it('测试中文翻译', () => {
+    expect(service.lang('ZH', '测试', 'user.userTest')).toBe('测试')
+  });
+
+  it('测试中文带参数翻译', () => {
+    expect(service.lang('ZH', '测试 {0}', 'user.userTestArgs', '哈哈')).toBe('测试 哈哈')
+  });
+
+  it('测试未定义翻译', () => {
+    expect(service.lang('ZH', '测试你好', 'user.userHello')).toBe('测试你好')
+  });
+
+  it('测试未定义带参数翻译', () => {
+    expect(service.lang('ZH', '测试{0}你好', 'user.userHello', '哈哈')).toBe('测试哈哈你好')
+  });
+
+  it('测试未定义带3个参数翻译', () => {
+    expect(service.lang('ZH', '测试{0}你好:{1},年龄:{2}', 'user.userHello', '哈哈', 'Oliver', 28)).toBe('测试哈哈你好:Oliver,年龄:28')
+  });
 });
