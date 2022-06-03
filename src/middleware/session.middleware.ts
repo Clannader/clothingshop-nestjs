b1d7021636a8b354eabf6f3d17064a8df4f154df
oliver <294473343@qq.com>
2022-06-03 14:50:09 +0800

    修改代码

--- src/middleware/session.middleware.ts
+++ src/middleware/session.middleware.ts
@@ -1,26 +1,20 @@
 /**
  * Create by CC on 2022/6/3
  */
-import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Response, NextFunction } from 'express';
 import { RequestSession, sessionName, Utils } from '../common';
 import * as cookieModule from 'cookie';
 
-@Injectable()
-export class ApiMiddleware implements NestMiddleware {
-  use(req: RequestSession, res: Response, next: NextFunction) {
-    const credential = req.headers['credential'];
-    const cookie = req.headers.cookie || '';
-    const cookieJSON = cookieModule.parse(cookie);
-    console.log(req.cookies);
-    console.log(cookie);
-    console.log(cookieJSON);
-    if (credential != null) {
-      // delete req.cookies[sessionName]
-      delete cookieJSON[sessionName];
-      cookieJSON[sessionName] = credential as string;
-    }
+export function SessionMiddleware(req: RequestSession, res: Response, next: NextFunction) {
+  const credential = req.headers['credential'];
+  const cookie = req.headers.cookie || ''; // 这个是原始的cookie取值
+  const cookieJSON = cookieModule.parse(cookie); // 原始的cookie是字符串,格式化变成JSON
+  // console.log(req.cookies); // 需要使用cookie-parser包才能有这个节点,并且是JSON格式的
+  if (credential != null) {
+    delete req.cookies[sessionName]
+    delete cookieJSON[sessionName];
+    cookieJSON[sessionName] = credential as string;
     req.headers.cookie = Utils.stringifyParams(cookieJSON, ';');
-    next();
   }
+  next();
 }
