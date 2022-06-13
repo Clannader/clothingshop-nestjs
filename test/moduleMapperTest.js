091ef4deae11c889ea142b68d2baeef3d76b2419
oliver <294473343@qq.com>
2022-06-13 19:07:39 +0800

    修改代码

--- test/moduleMapperTest.js
+++ test/moduleMapperTest.js
@@ -2,10 +2,15 @@
  * Create by CC on 2022/6/12
  * 用于转义别名路径的正则
  */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-console.log(pathsToModuleNameMapper({
-  "@/*":["src/*"],
-  "@T/*":["test/src/*"]
-}, {
-  // prefix: '这里可以加入前缀'
-}))
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const mapper = pathsToModuleNameMapper(
+  {
+    "@/*": ["src/*"],
+    "@T/*": ["test/src/*"]
+  },
+  {
+    // prefix: '<rootDir>/..', // 如果是test的运行使用这个前缀
+    // prefix: '<rootDir>/' // 如果是主代码的运行使用这个前缀
+  }
+);
+console.log(mapper);
