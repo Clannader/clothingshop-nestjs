f1568dad59dc3c0e70840e5ea91857e9acc59991
oliver <294473343@qq.com>
2022-07-21 18:22:08 +0800

    修改代码

--- src/common/decorator/session.decorator.ts
+++ src/common/decorator/session.decorator.ts
@@ -2,10 +2,11 @@
  * Create by CC on 2022/7/21
  */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { RequestSession } from '../common.types';
 
-export const AdminSession = createParamDecorator(
+export const UserSession = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request: RequestSession = ctx.switchToHttp().getRequest();
     return request.session.adminSession;
   },
 );
