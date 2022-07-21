8da39519517433d6c2e62de73ee9a675f907a672
oliver <294473343@qq.com>
2022-07-21 18:14:13 +0800

    修改代码

--- src/common/decorator/session.decorator.ts
+++ src/common/decorator/session.decorator.ts
@@ -3,7 +3,7 @@
  */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const CmsSession = createParamDecorator(
+export const AdminSession = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.session.adminSession;
