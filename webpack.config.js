1a09800c955b1a3370dde493a1d6e17f028a8744
oliver <294473343@qq.com>
2022-07-26 22:35:51 +0800

    修改代码

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = function(options) {
     ...options,
     output: {
       path: 'D:\\eclipseWorkspace\\clothingshop-nestjs\\build' // 暂时使用绝对路径,这个webpack打包还是有问题,估计缺少了插件,要不然不会写了无效的
-    //   ...options.output,
-    //   path: join(process.cwd(), 'build'),
-    //   // filename: join(process.cwd(), 'main.[chunkhash].js')
+      // ...options.output,
+      // path: join(process.cwd(), 'build'),
+      // filename: join(process.cwd(), 'main.[chunkhash].js')
     },
     optimization: {
       ...options.optimization,
