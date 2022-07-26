ed7ad5584b0def34ee7a0727a93e792f098b63dc
oliver <294473343@qq.com>
2022-07-26 10:37:43 +0800

    修改代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-import { join } from 'path'
+// import { join } from 'path'
 
 module.exports = function(options) {
   return {
     ...options,
-    output: {
-      path: join(process.cwd(), 'build'),
-      filename: join(process.cwd(), 'main.[chunkhash].js')
-    },
+    // output: {
+    //   path: join(process.cwd(), 'build'),
+    //   filename: join(process.cwd(), 'main.[chunkhash].js')
+    // },
     optimization: {
       ...options.optimization,
       minimize: process.env.NODE_ENV === 'production', // 可以对打包的js进行压缩
