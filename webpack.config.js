4f3b107d22ba0d3bf118b5cb0e51a49d54d8e48c
oliver <294473343@qq.com>
2022-07-26 10:50:48 +0800

    修改代码

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@
 module.exports = function(options) {
   return {
     ...options,
-    // output: {
-    //   path: join(process.cwd(), 'build'),
+    output: {
+      ...options.output,
+      path: 'D:\\eclipseWorkspace\\clothingshop-nestjs\\build',
     //   filename: join(process.cwd(), 'main.[chunkhash].js')
-    // },
+    },
     optimization: {
       ...options.optimization,
       minimize: process.env.NODE_ENV === 'production', // 可以对打包的js进行压缩
