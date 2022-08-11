eeeb7e805906b4944b78ab41e36d4bcde6d1b1a5
oliver <294473343@qq.com>
2022-08-11 08:32:24 +0800

    新增cache缓存管理代码

---
+++ src/cache/services/token.cache.service.ts
@@ -0,0 +1,16 @@
+/**
+ * Create by CC on 2022/8/11
+ */
+import { Injectable, Inject } from '@nestjs/common';
+import { caching, Cache } from 'cache-manager'
+
+@Injectable()
+export class TokenCacheService {
+
+  private readonly cacheManager: Cache
+
+  constructor() {
+    // @ts-ignore
+    this.cacheManager = caching()
+  }
+}
---
+++ src/cache/services/user.cache.service.ts
@@ -0,0 +1,8 @@
+/**
+ * Create by CC on 2022/8/11
+ */
+
+
+export class UserCacheService {
+
+}
