7e22652c86c2499f2a25a021263a98a9aa117663
oliver <294473343@qq.com>
2023-09-01 23:10:18 +0800

    修改代码

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,14 @@ module.exports = function(options) {
     output: {
       // ...options.output,
       path: path.join(process.cwd(), 'build'),
-      filename: 'main.[chunkhash].js'
+      // filename: 'main.[chunkhash].js'
+      filename: 'main.js'
     },
     optimization: {
       ...options.optimization,
       // nodeEnv: true,
       // minimize: false,
-      minimize: process.env.NODE_ENV === 'production', // 可以对打包的js进行压缩,这个也是有效的
+      // minimize: process.env.NODE_ENV === 'production', // 可以对打包的js进行压缩,这个也是有效的
     },
   };
 };
