b6c3f377a9824acf70b869a3536fa44adf7e30d0
oliver <294473343@qq.com>
2024-09-23 01:25:03 +0800

    新增数据库代码测试

--- src/system/system.service.ts
+++ src/system/system.service.ts
@@ -3,12 +3,31 @@ import { RespWebConfigDto, WebConfigDto } from './dto';
 import { ConfigService } from '@/common/config';
 import { CodeEnum } from '@/common/enum';
 
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection, Collection, Model } from 'mongoose';
+
+type ModelMap = {
+  modelName: string;
+  collectionName: string;
+};
+
+interface CustomCollection extends Collection {
+  modelName: string;
+}
+
+interface CustomModel extends Model<any> {
+  getAliasName(): string;
+}
+
 @Injectable()
 export class SystemService {
   @Inject()
   private readonly configService: ConfigService;
 
-  getSystemConfig(): RespWebConfigDto {
+  @InjectConnection()
+  private readonly mongooseConnection: Connection;
+
+  async getSystemConfig() {
     const resp = new RespWebConfigDto();
 
     const config = new WebConfigDto();
@@ -22,6 +41,43 @@ export class SystemService {
     resp.config = config;
     resp.code = CodeEnum.SUCCESS;
 
+    // 这个是数据库总的状态
+    // const stats = await this.mongooseConnection.db.stats()
+    // console.log(stats)
+
+    // 这个是数据库总的信息
+    // const serverInfo = await this.mongooseConnection.db.admin().serverInfo()
+    // console.log(serverInfo)
+
+    const modelMap = this.getModelMap()
+    for (const [aliasName, modelInfo] of modelMap) {
+      // console.log(aliasName)
+      const name = modelInfo.collectionName
+      const collStats = await this.mongooseConnection.collection(name).stats()
+      console.log(`ns: ${collStats.ns}`)
+      console.log(`size: ${collStats.size}`)
+      console.log(`storageSize: ${collStats.storageSize}`)
+      console.log(`count: ${collStats.count}`)
+      console.log(`avgObjSize: ${collStats.avgObjSize}`)
+    }
+
     return resp;
+  }
+
+  private getModelMap(): Map<string, ModelMap> {
+    const modelMap = new Map<string, ModelMap>();
+    const collections = this.mongooseConnection.collections; // 所有连接名
+    for (const collection in collections) {
+      const value = collections[collection] as CustomCollection;
+      const modelName = value.modelName;
+      const aliasName = (
+        this.mongooseConnection.models[modelName] as CustomModel
+      ).getAliasName();
+      modelMap.set(aliasName, {
+        collectionName: value.collectionName,
+        modelName,
+      });
+    }
+    return modelMap;
   }
 }
