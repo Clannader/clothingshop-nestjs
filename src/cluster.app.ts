09b89d16a9c2751695d442988d72163dbe87a426
AndyBeat <137811032@qq.com>
2024-09-25 14:41:50 +0800

    新增使用多进程启动服务器配置

---
+++ src/cluster.app.ts
@@ -0,0 +1,43 @@
+/**
+ * Create by oliver.wu 2024/9/25
+ */
+import cluster from 'cluster';
+import { availableParallelism } from 'node:os';
+import parseEnv from '@/lib/parseEnv';
+import { bootstrap } from './single.app';
+
+export async function clusterApp() {
+  let numCPUs = availableParallelism();
+  console.log(cluster)
+  if (cluster.isPrimary) {
+    console.log(`Master Pid:${process.pid} is running`);
+    // 根据config.ini的进程数量开启多少个进程
+    const threadNum = parseEnv.read('threadNum');
+    if (!isNaN(threadNum)) {
+      numCPUs = +threadNum;
+    }
+
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', function (worker, code) {
+      //worker, code, signal
+      //之所以打印2次监听是因为开启了http和https服务导致的
+      console.log(
+        `Sub-thread-worker ID:${worker.id} exit, processID : ${worker.process.pid}`,
+      );
+      console.log(`Sub-thread-code Code:${code}`);
+    });
+
+    cluster.on('listening', function (worker) {
+      //worker, address
+      console.log(
+        `Sub-thread-worker ID:${worker.id} listening, processID : ${worker.process.pid}`,
+      );
+    });
+  } else {
+    await bootstrap();
+    console.log(`Worker ID:${cluster.worker.id} started, Pid: ${process.pid}`);
+  }
+}
