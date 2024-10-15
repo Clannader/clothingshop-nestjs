09bdc3702c856bd113c7a7a9408b91c55267d8b7
AndyBeat <137811032@qq.com>
2024-10-15 14:02:03 +0800

    新增时区功能

--- src/tasks/services/test.tasks.service.ts
+++ src/tasks/services/test.tasks.service.ts
@@ -4,8 +4,9 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 
-import { SequenceTypeEnum } from '@/common/enum';
-import { SequenceSchemaService } from '@/entities/services';
+import { LogTypeEnum, SequenceTypeEnum } from '@/common/enum';
+import { AdminLogSchemaService, SequenceSchemaService } from '@/entities/services';
+import type { AdminLog } from '@/entities/schema';
 
 // @ts-ignore
 const cluster = require('node:cluster');
@@ -14,6 +15,9 @@ const cluster = require('node:cluster');
 export class TestTasksService {
   @Inject()
   private readonly sequenceSchemaService: SequenceSchemaService;
+
+  @Inject()
+  private readonly adminLogSchemaService: AdminLogSchemaService;
 
   @Interval(3000)
   async handleInterval() {
@@ -32,5 +36,14 @@ export class TestTasksService {
     console.log(
       `服务器ID: ${cluster.worker ? cluster.worker.id : 1}, 序列号为:${result.sequenceId}`,
     );
+    const logInfo: AdminLog = {
+      adminId: 'SYSTEM',
+      adminName: 'Supervisor',
+      content: `服务器ID: ${cluster.worker ? cluster.worker.id : 1}, 序列号为:${result.sequenceId}`,
+      shopId: 'SYSTEM',
+      type: LogTypeEnum.Config,
+      traceId: Date.now().toString(),
+    };
+    await this.adminLogSchemaService.createUserLog(logInfo)
   }
 }
