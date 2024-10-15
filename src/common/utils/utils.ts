d2188b95e1526ef54a7321a0ec722df6ddd528d9
AndyBeat <137811032@qq.com>
2024-10-15 17:21:53 +0800

    新增简化async await的写法方法

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -513,4 +513,19 @@ export class Utils {
     // }
     return this.replaceArgs(langKey, ...args);
   }
+
+  static async toPromise<T, U = Error>(
+    promise: Promise<T>,
+    errorExt?: object,
+  ): Promise<[U, undefined] | [null, T]> {
+    return await promise
+      .then<[null, T]>((data: T) => [null, data])
+      .catch<[U, undefined]>((err: U) => {
+        if (errorExt) {
+          const parsedError = Object.assign({}, err, errorExt);
+          return [parsedError, undefined];
+        }
+        return [err, undefined];
+      });
+  }
 }
