d5845708b8bf13e5ba29deca6f62e9119eaaef44
AndyBeat <137811032@qq.com>
2024-10-22 16:27:49 +0800

    虚拟字段定义方法

--- src/entities/schema/system/data/timeZone.schema.ts
+++ src/entities/schema/system/data/timeZone.schema.ts
@@ -31,6 +31,16 @@ export class TimeZoneData extends CommonData {
     match: timeZoneExp,
   })
   winter: string; // 冬令时
+
+  // 只有使用@Prop()定义的字段才会存到数据库中
+  // 没有@Prop()时的字段是不会存到数据库的
+  // @Prop()和虚拟字段不能冲突
+  // 如果定义虚拟字段时,又定义@Prop()会提示报错
+  // winterLabel: string;
+
+  // 虚拟字段定义例子,直接在该类上定义字段名和类型即可,mongoose默认每个表都加上了虚拟字段id=_id.toString(),所以不用定义
+  // 如果是其他虚拟字段的,则按下面的方式定义即可
+  // id: string;
 }
 
 export type TimeZoneDataDocument = HydratedDocument<TimeZoneData>;
