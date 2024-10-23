52a63d1de14c854b5fea4d9d9b0c3f73bd0fb795
AndyBeat <137811032@qq.com>
2024-10-23 09:03:22 +0800

    修改错别字

--- src/logs/services/userLogs.service.ts
+++ src/logs/services/userLogs.service.ts
@@ -26,7 +26,7 @@ export class UserLogsService {
   ) {
     // linkId定义Array<string>或者string[],数据库字段type: Array时,如果传入any类型,实际上是字符串,则会以字符串存储
     // linkId定义Array<string>或者string[],数据库字段type: [String]时,如果传入any类型,实际上是字符串,则会以数组存储
-    // 但是为了能准备表达传入的类型和存储类型正确,定义Array<string> | string
+    // 但是为了能准确表达传入的类型和存储类型正确,定义Array<string> | string
     const linkIdObj: LinkIdObj = {};
     if (linkId.length > 0) {
       if (Array.isArray(linkId)) {
