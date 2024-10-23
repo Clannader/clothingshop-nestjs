70114d811b1baa1761c978c03bb924b11c03510b
AndyBeat <137811032@qq.com>
2024-10-23 14:22:48 +0800

    新增翻译

--- src/common/i18n/timeZone/timeZone.zh.ts
+++ src/common/i18n/timeZone/timeZone.zh.ts
@@ -4,4 +4,8 @@
 export const timeZone = {
   isNotExist: '该时区不存在',
   deleteLog: '删除时区:({0})',
+  timeZoneLabel: '时区名称',
+  summerLabel: '夏令时',
+  winterLabel: '冬令时',
+  modifiedLog: '编辑时区:({0})'
 };
