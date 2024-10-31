e929529a78a252022f8e4f0a1fb2a4f669062b28
AndyBeat <137811032@qq.com>
2024-10-31 10:54:57 +0800

    修改ts断言

--- src/cluster.app.ts
+++ src/cluster.app.ts
@@ -1,7 +1,9 @@
 /**
  * Create by oliver.wu 2024/9/25
  */
-// import * as cluster from 'node:cluster'; // 用这个声明,使用时ts编译不过去
+// 这样写是可以的,但是ts校验类型又过不去
+// import * as cluster from 'node:cluster';
+import type { Worker } from 'node:cluster'; // 用这个声明,使用时ts编译不过去
 // @ts-ignore
 const cluster = require('node:cluster'); // 用这个声明的话,又不是ts的写法,暂时这样弄吧
 import { availableParallelism } from 'node:os';
@@ -24,7 +26,7 @@ export async function clusterApp() {
       cluster.fork();
     }
 
-    cluster.on('exit', function (worker, code) {
+    cluster.on('exit', function (worker: Worker, code: number) {
       //worker, code, signal
       //之所以打印2次监听是因为开启了http和https服务导致的
       console.log(
@@ -33,7 +35,7 @@ export async function clusterApp() {
       console.log(`Sub-thread-code Code:${code}`);
     });
 
-    cluster.on('listening', function (worker) {
+    cluster.on('listening', function (worker: Worker) {
       //worker, address
       console.log(
         `Sub-thread-worker ID:${worker.id} listening, processID : ${worker.process.pid}`,
