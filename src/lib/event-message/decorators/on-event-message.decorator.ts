1e59754efb163bb91a3454f41b49aee9df625bf1
AndyBeat <137811032@qq.com>
2024-12-05 11:20:07 +0800

    新增封装 发和收 进程消息方式

--- src/lib/event-message/decorators/on-event-message.decorator.ts
+++ src/lib/event-message/decorators/on-event-message.decorator.ts
@@ -1,8 +1,9 @@
 /**
  * Create by oliver.wu 2024/12/3
  */
+import { applyDecorators, SetMetadata } from '@nestjs/common';
 import { extendArrayMetadata } from '@nestjs/common/utils/extend-metadata.util';
-import { EVENT_MESSAGE_METADATA } from '../constants';
+import { EVENT_ON_MESSAGE_METADATA, EVENT_SEND_MESSAGE_METADATA } from '../constants';
 
 export type OnEventMessageType = string | symbol;
 
@@ -11,7 +12,7 @@ export interface OnEventMessageMetadata {
 }
 
 export const OnEventMessage = (
-  message?: OnEventMessageType,
+  message: OnEventMessageType,
 ): MethodDecorator => {
   const decoratorFactory = (
     target: object,
@@ -19,12 +20,20 @@ export const OnEventMessage = (
     descriptor?: TypedPropertyDescriptor<any>,
   ) => {
     extendArrayMetadata(
-      EVENT_MESSAGE_METADATA,
+      EVENT_ON_MESSAGE_METADATA,
       [{ message } as OnEventMessageMetadata],
       descriptor!.value as (...args: any[]) => any,
     );
     return descriptor;
   };
-  decoratorFactory.KEY = EVENT_MESSAGE_METADATA;
+  decoratorFactory.KEY = EVENT_ON_MESSAGE_METADATA;
   return decoratorFactory;
 };
+
+export const SendEventMessage = (
+  message: OnEventMessageType,
+): MethodDecorator => {
+  return applyDecorators(
+    SetMetadata(EVENT_SEND_MESSAGE_METADATA, message)
+  )
+}
