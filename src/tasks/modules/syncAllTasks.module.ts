195bf26367cbb8d82150565fdd8548a4cf3b12da
AndyBeat <137811032@qq.com>
2024-12-24 11:35:33 +0800

    修改自己修改过的发布的包

--- src/tasks/modules/syncAllTasks.module.ts
+++ src/tasks/modules/syncAllTasks.module.ts
@@ -2,12 +2,9 @@
  * Create by oliver.wu 2024/11/5
  */
 import { Module } from '@nestjs/common';
-
-import { IntervalsTaskNameModule } from '@/lib/intervals-task-name';
 import { SyncAllTasksService } from '../services';
 
 @Module({
-  imports: [IntervalsTaskNameModule],
   providers: [SyncAllTasksService],
 })
 export class SyncAllTasksModule {}
