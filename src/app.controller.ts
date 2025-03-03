72a4c5a92c29bf48abe57629a9e7f67011a58595
AndyBeat <137811032@qq.com>
2025-03-03 11:12:41 +0800

    修改nestjs新版本导致的/index进不去问题

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,9 @@ import { ApiExcludeController } from '@nestjs/swagger';
 @Controller('/')
 @ApiExcludeController(true) // 排除这controller不在swagger显示
 export class AppController {
-  @Get(['/index$', '/index/*'])
+  // @Get(['/index$', '/index/*']) // 旧版本的需要加上$,否则/indexTest这样的路径也能进去首页
+  // 新版本的已经不会进去了,所以需要去掉$符号
+  @Get(['/index', '/index/*'])
   @Render('index')
   gotoIndex() {
     return {};
