3dd5d3e52eb5f4682f046117251cdd2fb5be31b6
AndyBeat <137811032@qq.com>
2025-08-06 11:31:15 +0800

    新增加密配置逻辑

--- src/entities/schema/secret.schema.ts
+++ src/entities/schema/secret.schema.ts
@@ -2,6 +2,7 @@
  * Create by oliver.wu 2024/11/12
  */
 import { Prop } from '@nestjs/mongoose';
+import { WriteLog } from '@/common/decorator';
 
 export class SecretSchema {
   @Prop({
@@ -9,12 +10,21 @@ export class SecretSchema {
     required: true,
     trim: true,
   })
+  @WriteLog({
+    origin: '秘钥ID',
+    key: 'system.parameterSecretId',
+  })
   secretId: string; // 关联秘钥的ID
 
   @Prop({
     type: String,
     required: true,
     trim: true,
+  })
+  @WriteLog({
+    origin: '密文',
+    key: 'system.parameterSecretText',
+    piiData: true,
   })
   secretText: string; // 加密后的密文
 }
