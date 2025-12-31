1e9c3d979dfcc83554418d15d733509675440110
AndyBeat <137811032@qq.com>
2025-12-31 09:55:49 +0800

    修改swagger

--- src/rights/dto/rightsGroup/req/req-rightsGroup-modify.dto.ts
+++ src/rights/dto/rightsGroup/req/req-rightsGroup-modify.dto.ts
@@ -1,5 +1,17 @@
 /**
  * Create by oliver.wu 2025/12/26
  */
+import { CreateRightsGroupDto } from '../create-rightsGroup.dto';
+import { PartialType } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsDefined, IsString } from 'class-validator';
 
-export class ReqRightsGroupModifyDto {}
+export class ReqRightsGroupModifyDto extends PartialType(CreateRightsGroupDto){
+  /**
+   * 编辑的ID值
+   */
+  @IsDefined()
+  @IsString()
+  @Expose()
+  id: string;
+}
--- src/rights/dto/rightsGroup/resp/resp-rightsGroup-search.dto.ts
+++ src/rights/dto/rightsGroup/resp/resp-rightsGroup-search.dto.ts
@@ -1,5 +1,17 @@
 /**
  * Create by oliver.wu 2025/12/26
  */
+import { ListRightsGroupDto } from '../list-rightsGroup.dto';
+import { CommonResult } from '@/common';
 
-export class RespRightsGroupSearchDto {}
+export class RespRightsGroupSearchDto extends CommonResult{
+  /**
+   * 权限组列表
+   */
+  rightsGroups: ListRightsGroupDto[];
+
+  /**
+   * 总数
+   */
+  total: number;
+}
