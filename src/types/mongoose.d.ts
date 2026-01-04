74e9729b983dbdeba767ee990df462a1a3138913
AndyBeat <137811032@qq.com>
2026-01-04 16:41:17 +0800

    新增mongoose全局静态方法

--- src/types/mongoose.d.ts
+++ src/types/mongoose.d.ts
@@ -3,10 +3,9 @@
  */
 // src/types/mongoose.d.ts
 import 'mongoose';
-import { OrgModel as Model, HydratedDocument } from 'mongoose';
 
 declare module 'mongoose' {
-  interface Model extends OrgModel{
+  interface Model {
     // 根据你的方法签名调整参数/返回类型
     syncSaveDBObject<T extends HydratedDocument>(
       dbDataDocs: HydratedDocument<T>,
