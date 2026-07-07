a1ffe1bbda4e4e273e8c9592d949dc6a0c849ad3
AndyBeat <137811032@qq.com>
2026-07-07 14:47:46 +0800

    新增编辑主文档逻辑

--- src/subRecord/dto/subRecord-ModifyMaster.dto.ts
+++ src/subRecord/dto/subRecord-ModifyMaster.dto.ts
@@ -1,19 +1,13 @@
 /**
  * Create by oliver.wu 2026/7/7
  */
-import { PartialType } from '@nestjs/swagger';
 import { SubRecordCreateMasterDto } from './subRecord-CreateMaster.dto';
 import { Expose } from 'class-transformer';
-import { IsDefined, IsMongoId } from 'class-validator';
 
-export class SubRecordModifyMasterDto extends PartialType(
-  SubRecordCreateMasterDto,
-) {
+export class SubRecordModifyMasterDto extends SubRecordCreateMasterDto {
   /**
    *  数据的ID
    */
   @Expose()
-  @IsDefined()
-  @IsMongoId()
   id: string;
 }
