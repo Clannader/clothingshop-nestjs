31fbacd5abfe503273c481c6521d1dba7fa429b5
AndyBeat <137811032@qq.com>
2026-08-20 13:03:57 +0800

    新增pms2配置例子

--- src/logger/log4js.logger.ts
+++ src/logger/log4js.logger.ts
@@ -23,7 +23,8 @@ function replaceConsole(logger: Log4js.Logger) {
 
 Log4js.configure({
   pm2: true, // 必须设置为 true
-  pm2InstanceVar: 'INSTANCE_ID', // 需与 PM2 配置中的 instance_var 对应
+  disableClustering: true,
+  // pm2InstanceVar: 'INSTANCE_ID', // 需与 PM2 配置中的 instance_var 对应
   appenders: {
     fileLogs: {
       type: 'file',
