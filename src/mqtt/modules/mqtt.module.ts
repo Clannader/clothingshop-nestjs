e03dbec4f51c90442f333049f9b8dc8e28f459b7
AndyBeat <137811032@qq.com>
2026-09-23 17:10:56 +0800

    集群模式下的mqtt clientId设置

--- src/mqtt/modules/mqtt.module.ts
+++ src/mqtt/modules/mqtt.module.ts
@@ -6,6 +6,7 @@ import { MqttAbstractService, normalizeMqttConfig } from '../services';
 import { ConfigService } from '@/common/config';
 import { SecretConfigModule } from '@/common/modules';
 import { SECRET_CONFIG, GLOBAL_CONFIG } from '@/common';
+import cluster from 'node:cluster';
 
 @Module({
   imports: [SecretConfigModule.register()],
@@ -18,12 +19,15 @@ import { SECRET_CONFIG, GLOBAL_CONFIG } from '@/common';
         configService: ConfigService,
         secretConfig: ConfigService,
       ) => {
+        const workerId = cluster?.worker?.id ?? 1;
+        const serverName = configService.get<string>('serverName')
+        const clientId = secretConfig.get<string>('mqttClientId')
         const svc = new MqttAbstractService(
           normalizeMqttConfig({
             brokerUrl: configService.get<string>('mqttUrl'),
             username: secretConfig.get<string>('mqttUserName'),
             password: secretConfig.get<string>('mqttPassword'),
-            clientId: secretConfig.get<string>('mqttClientId'), // clientId就是连接的名称,如果不设置就是随机
+            clientId: `${clientId}-${serverName}-${workerId}`, // clientId就是连接的名称,如果不设置就是随机
           }),
         );
         svc.start();
